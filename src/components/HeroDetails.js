588d6a6e6d5a8e149fed6af43d8ae340db07742b
PoweRGbg <roumen_georgiev@yahoo.com>
2022-01-05 16:12:10 +0200

    updating hero stats 2

--- src/components/HeroDetails.js
+++ src/components/HeroDetails.js
@@ -1,7 +1,6 @@
 import AuthContext from "../contexts/AuthContext";
 import { useEffect, useState, useContext } from "react";
-import { getHeroById, deleteHero } from "../services/heroService";
-import CommentsCard from "./CommentsCard";
+import { getHeroById, deleteHero, editHero } from "../services/heroService";
 import ConfirmDialog from "./ConfirmDialog.js";
 import { useHistory } from "react-router-dom";
 import "../css/fontawesome.min.css";
@@ -9,11 +8,11 @@ import "../css/bootstrap.min.css";
 import "../css/templatemo-style.css";
 
 export default function HeroDetails({ match }) {
-  const [hero, setHero] = useState([]);
+  const [hero, setHero] = useState({});
+  const [rerender, setRerender] = useState();
   const { user } = useContext(AuthContext);
   let historyHook = useHistory();
   const [note, setNote] = useState("");
-  const [toDelete, setToDelete] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -22,22 +21,36 @@ export default function HeroDetails({ match }) {
     }
     fetchData()
       .then((result) => {
-        if (!result.message && !Array.isArray(result.ingredients) ) {
-          result.ingredients = result.ingredients.split("\r");
-        }
         setHero(result);
       })
       .then(window.scrollTo(0, 0));
-  }, [match.params.heroId, user]);
+  }, [match.params.heroId, rerender]);
+
+  function saveHero() {
+    editHero(hero, user).then((updatedHero) => {
+      console.log(`Updated is ${updatedHero}`);
+      setHero(updatedHero);
+
+      setRerender(rerender + 1);
+    });
+    getHeroById(hero._id)
+      .then((result) => {
+        setHero(result);
+      });
+  }
 
   function editButtonHandler(e) {
     e.preventDefault();
     historyHook.push(`/edit/${hero._id}`);
   }
 
+  function trainButtonHandler(e) {
+    e.preventDefault();
+    historyHook.push(`/arena/${hero._id}`);
+  }
+
   function deleteButtonHandler(e) {
     e.preventDefault();
-    setToDelete(hero);
     setNote(`Are you sure you want to remove hero ${hero.name}?`);
   }
 
@@ -48,9 +61,15 @@ export default function HeroDetails({ match }) {
 
   async function deleteHandler() {
     await deleteHero(hero, user);
-    historyHook.push(`/heros/myheros`);
+    historyHook.push(`/heroes/myheroes`);
   }
 
+   function plusButtonHandler(what, howMuch, e) {
+    console.log(`Plus button for ${what} ${howMuch}`);
+    let upgradedHero = hero;
+    upgradedHero[what] += howMuch;
+    saveHero();
+  }
   const ownerButtons = (
     <div className="col-6">
       <button
@@ -69,9 +88,20 @@ export default function HeroDetails({ match }) {
       </button>
     </div>
   );
+
+  const trainButton = (
+    <div className="col-6">
+      <button
+        type="submit"
+        className="btn btn-primary btn-block text-uppercase"
+        onClick={trainButtonHandler}
+      >
+        Train
+      </button>
+    </div>
+  );
+
   return hero.name === undefined ? (
-
-
     <div className="container tm-mt-big tm-mb-big">
       <div className="row">
         <div className="col-xl-9 col-lg-10 col-md-12 col-sm-12 mx-auto">
@@ -92,66 +122,113 @@ export default function HeroDetails({ match }) {
     </div>
   ) : (
     <>
-    <ConfirmDialog show={note} onClose={() => setNote(false)} onSave={deleteHandler} text={note} />
-    <div className="container tm-mt-big tm-mb-big">
-      <div className="row">
-        <div className="col-xl-9 col-lg-10 col-md-12 col-sm-12 mx-auto">
-          <div className="tm-bg-primary-dark tm-block tm-block-h-auto">
-            <div className="row"></div>
-            <div className="row tm-edit-product-row">
-              <div className="tm-product-img-edit mx-auto">
-                <img
-                  src={hero.imageURL}
-                  alt="HeroShot"
-                  className="img-fluid d-block mx-auto"
-                />
-              </div>
-              <div className="col-xl-6 col-lg-6 col-md-12">
-                <div className="form-group mb-3">
-                  <label htmlFor="name">Hero Name</label>
-                  <h4 className="form-control">{hero.name}</h4>
-                </div>
-                <div className="form-group mb-3">
-                  <label htmlFor="description">Description</label>
-                  <h5 className="form-control">{hero.description}</h5>
-                </div>
-                <div className="form-group mb-3">
-                  <label htmlFor="ingredients">Ingredients needed</label>
-                  <ul className="form-control">
-                    {hero.ingredients.map((ingredient) => (
-                      <li key={ingredient.length+Date.now()}>{ingredient}</li>
-                    ))}
-                  </ul>
-                </div>
-                <div className="form-group mb-3">
-                  <label htmlFor="recipe">Preparation</label>
-                  <h5 className="form-control">{hero.recipe}</h5>
-                </div>
+      <ConfirmDialog
+        show={note}
+        onClose={() => setNote(false)}
+        onSave={deleteHandler}
+        text={note}
+      />
+      <div className="container tm-mt-big tm-mb-big">
+        <div className="row">
+          <div className="col-xl-9 col-lg-10 col-md-12 col-sm-12 mx-auto">
+            <div className="tm-bg-primary-dark tm-block tm-block-h-auto">
+              <div className="row"></div>
+              <div className="row tm-edit-product-row">
+                <div className="col-xl-6 col-lg-6 col-md-12">
+                  <div className="form-group mb-3">
+                    <label htmlFor="name">Hero Name</label>
+                    <h4 className="form-control">{hero.name}</h4>
+                  </div>
+                  <div className="form-group mb-3">
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td>Life</td>
+                          <td className="form-control">{hero.life}</td>
+                        </tr>
+                        <tr>
+                          <td>Attack</td>
+                          <td className="form-control">
+                            {hero.attack}
+                            <button
+                              type="submit"
+                              className="btn btn-primary btn-block text-uppercase col-6"
+                              onClick={e => { plusButtonHandler("attack",1, e)}}
+                            >
+                              +
+                            </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Defense</td>
+                          <td className="form-control">
+                            {hero.defense}
+                            <button
+                              type="submit"
+                              className="btn btn-primary btn-block text-uppercase col-6"
+                              onClick={e => { plusButtonHandler("defense",1, e)}}
+                            >
+                              +
+                            </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Experience</td>
+                          <td className="form-control">
+                            {hero.xp}
+                            <button
+                              type="submit"
+                              className="btn btn-primary btn-block text-uppercase col-6"
+                              onClick={(e) => {
+                                plusButtonHandler("xp", 1, e);
+                              }}
+                            >
+                              +
+                            </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>Gold</td>
+                          <td className="form-control">
+                            {hero.gold}
+                            <button
+                              type="submit"
+                              className="btn btn-primary btn-block text-uppercase col-6"
+                              onClick={(e) => {
+                                plusButtonHandler("gold", 20, e);
+                              }}
+                            >
+                              +
+                            </button>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
 
-                <div className="row">
-                  <div className="custom-file mt-3 mb-3">
-                    {hero._ownerId === user._id ? ownerButtons : ""}
+                  <div className="row">
+                    <div className="custom-file mt-3 mb-3">
+                      {hero._ownerId === user._id ? ownerButtons : ""}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <CommentsCard hero={hero} />
 
-          <div className="container tm-mt-big tm-mb-big">
-            <div className="row custom-file mt-3 mb-3 col-3">
-              <button
-                type="submit"
-                className="btn btn-primary btn-block text-uppercase col-6"
-                onClick={goBackHandler}
-              >
-                Go back
-              </button>
+            <div className="container tm-mt-big tm-mb-big">
+              <div className="row custom-file mt-3 mb-3 col-3">
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-block text-uppercase col-6"
+                  onClick={goBackHandler}
+                >
+                  Go back
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
