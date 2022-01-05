9f4b1d71a7e4419705b8e85008c5e82cdacf569c
PoweRGbg <roumen_georgiev@yahoo.com>
2022-01-05 16:11:48 +0200

    updating hero stats works

--- src/services/heroService.js
+++ src/services/heroService.js
@@ -30,8 +30,9 @@ export const addHero = async (hero, user) => {
 
 }
 export const editHero = async (hero, user) => {
+    console.log(`Updating ${JSON.stringify(hero)}`);
     try {
-        let response = await fetch(url + "data/heroes/"+hero._id, {
+        let response = await fetch(url + "update", {
             method: 'PUT',
             headers: {
                 'content-type': 'application/json',
@@ -40,17 +41,7 @@ export const editHero = async (hero, user) => {
             body: JSON.stringify(hero)
         });
         let result = await response.json();
-        // Notification for Edit
-        let notification =
-        {
-            who: user.email,
-            dateString: Date.now().toString(),
-            date: Date.now(),
-            text: `Editted `,
-            recipe: hero._id,
-            recipeName: hero.name,
-        }
-        await addNotification(notification);
+        console.log(`Result is ${result}`);
         return result;
     } catch (error) {
         console.error(error)
